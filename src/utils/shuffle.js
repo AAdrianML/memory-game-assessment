fa3c6b12e3cd86e80eee3f4e54fcf24d0d2412b4
Adrian Madrigal <trabajo.aaml@gmail.com>
2026-05-13 15:33:08 -0700

    fix: replace biased random sort shuffle with Fisher-Yates algorithm

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,3 +1,16 @@
 export const shuffleArray = array => {
-    return [...array].sort(() => Math.random() - 0.5)
+  const shuffled = [...array]
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(
+      Math.random() * (i + 1)
+    )
+
+    ;[shuffled[i], shuffled[randomIndex]] = [
+      shuffled[randomIndex],
+      shuffled[i],
+    ]
   }
+
+  return shuffled
+}
